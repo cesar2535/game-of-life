23838b8968c0bab89b9d4f35739e221fa63b57bb
Cesar Chen <cesar6134@gmail.com>
2019-08-19 01:13:21 +0800

    Updates

---
+++ precache-manifest.e2cda9947dcbb9b23c0f875ef389e2a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec6caa5644c5e884fa70734d1c439bce",
+    "url": "/cesar2535/game-of-life/index.html"
+  },
+  {
+    "revision": "a5a43a4b7364f4f78d3e",
+    "url": "/cesar2535/game-of-life/static/css/main.42cabb35.chunk.css"
+  },
+  {
+    "revision": "12ebf1a0b409e94d4d7a",
+    "url": "/cesar2535/game-of-life/static/js/2.a37d104d.chunk.js"
+  },
+  {
+    "revision": "a5a43a4b7364f4f78d3e",
+    "url": "/cesar2535/game-of-life/static/js/main.0606c5d6.chunk.js"
+  },
+  {
+    "revision": "4a629fba159782904046",
+    "url": "/cesar2535/game-of-life/static/js/runtime~main.f70e34ce.js"
+  }
+]);
