8fb3e14dcc204e2fb842b801efb6d239a6472a01
Cesar Chen <cesar6134@gmail.com>
2023-11-03 00:45:47 +0800

    feat: upgrade react to 18

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function main() {
+  const element = document.getElementById("root");
+  const root = createRoot(element);
+
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
+
+main();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
